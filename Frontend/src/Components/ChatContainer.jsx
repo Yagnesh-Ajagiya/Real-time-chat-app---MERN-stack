1a7391774cf29f2e567e9aa7a2c4284ac31261fa
Yagnesh06 <yagneshajagiya29@gmail.com>
2025-02-08 13:19:55 +0530

    change

--- Frontend/src/Components/ChatContainer.jsx
+++ Frontend/src/Components/ChatContainer.jsx
@@ -1,9 +1,8 @@
 import { useChatStore } from "../store/useChatStore";
 import { useEffect, useRef } from "react";
-
-import ChatHeader from "./ChatHeader";
-import MessageInput from "./MessageInput";
-import MessageSkeleton from "./skeletons/MessageSkeleton";
+import ChatHeader from "./ChatHeader"
+import MessageInput from "./MessageInput"
+import MessageSkeleton from "../Components/Skeletons/MessageSkeleton"
 import { useAuthStore } from "../store/useAuthStore";
 import { formatMessageTime } from "../lib/utils";
 
